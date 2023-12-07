5b000fd87b3c5ec55a4fdc14c0174027d37bbd2f
Md-Fahim-Shahriar <2120552@iub.edu.bd>
2023-12-08 01:25:32 +0600

    final touch done

--- allBooks/allBooks.php
+++ allBooks/allBooks.php
@@ -26,7 +26,7 @@ $conn->close();
     <meta charset="UTF-8"/>
     <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-    <link rel="stylesheet" href="homepage.css"/>
+    <link rel="stylesheet" href="allBooks.css"/>
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&family=Sen:wght@400;700;800&display=swap" rel="stylesheet"/>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"/>
     <title>All Books</title>
@@ -38,12 +38,10 @@ $conn->close();
         <div class="logo-container"><h1 class="logo">Vocable</h1></div>
         <div class="menu-container">
             <ul class="menu-list">
-                <li class="menu-list-item active">Home</li>
-                <li class="menu-list-item">Books</li>
-                <li class="menu-list-item">Contact</li>
+                <a style="color: aliceblue;" href="../homepage/homepage.php"><li class="menu-list-item">Home</li></a>
+                <a style="color: aliceblue;" href=""><li class="menu-list-item active">Books</li></li></a>
+                <a style="color: aliceblue;" href="../contact/contact.php"><li class="menu-list-item">Contact</li></a>
             </ul>
-        </div>
-        <!-- profile -->            
         </div>
         <div class="profile-container">
             <img class="profile-picture" src="images/user.jpg" alt=""/>
@@ -51,8 +49,8 @@ $conn->close();
                 <span class="profile-text">Profile</span>
                 <i class="fas fa-caret-down" style="cursor: pointer;">
                     <div class="dropdown-menu">
-                        <a href="#">My Profile</a>
-                        <a href="#">Sign Out</a>
+                        <a href="../profile/profile.php">My Profile</a>
+                        <a href="../index/index.html">Sign Out</a>
                     </div>
                 </i>
             </div>
@@ -65,19 +63,9 @@ $conn->close();
 <!-- container -->
 <div class="container">
     <div class="content-container">
-        <!-- <div
-                class="featured-content"
-                style="
-            background: linear-gradient(to bottom, rgba(0, 0, 0, 0), #6b808b),
-              url(images/Elevate\ Your\ Reading\ Experience\,\ One\ Word\ at\ a\ Time!.png);
-            background-size: cover;
-          "
-        >
-        </div> -->
-
         <!-- All container -->
         <div class="book-list-container" style="align-items: center;">
-            <h1 class="book-list-title">All Books</h1>
+            <h1 class="book-list-title" style="text-align: relative;">All Books</h1>
             <div class="book-list-wrapper">
                 <div class="book-list">
                     <?php
