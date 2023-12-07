45cd148596a4afa500a1bbeb89c3dc08be5ac7a3
Md-Fahim-Shahriar <2120552@iub.edu.bd>
2023-12-08 00:16:05 +0600

    allBooks Branch First Commit

---
+++ allBooks/test.php
@@ -0,0 +1,3 @@
+<img class="book-list-item-img" src="books-img/<?php echo $row['Image']; ?>" alt="<?php echo $row['Name']; ?>"/>
+                            <span class="book-list-item-title"><?php echo $row['Name']; ?></span>
+                            <button class="book-list-item-button">Learn Words</button>
