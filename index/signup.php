6ac97c2c2701f14894a9597fe051dd8f96e0ee9c
Md-Fahim-Shahriar <2120552@iub.edu.bd>
2023-12-07 19:03:25 +0600

    Moved everything to Index folder

---
+++ index/signup.php
@@ -0,0 +1,18 @@
+<?php
+
+$link = mysqli_connect('localhost', 'root', '', 'webappdb');
+
+$name_insert = $_POST["userNameSU"];
+$email_insert = $_POST["userEmailSU"];
+$pass_insert = $_POST["userPassSU"];
+
+// Insert data into the database
+$sql = "INSERT INTO user (userName, userEmail, userPass) VALUES ('$name_insert', '$email_insert', '$pass_insert')";
+
+if (mysqli_query($link, $sql) == TRUE) {
+    echo '<script>alert("User Has Been Added");</script>';
+} else {
+    echo '<script>alert("Error Occured");</script> ';
+}
+mysqli_close($link);
+?>
